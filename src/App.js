7c7329b1d9282f7a08ae710f5aefd968bb41d749
dkag525 <dkag525@gmail.com>
2024-02-10 22:09:11 +0530

    BankDash

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LeftNavBar from "./components/Navbar/LeftNavBar/LeftNavBar";
+import TopNavBar from "./components/Navbar/TopNavBar/TopNavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="layout-nav">
+        <LeftNavBar />
+        <TopNavBar />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar/TopNavBar/TopNavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../TopNavBar/TopNavBar.css";
+
+const TopNavBar = () => {
+  return (
+    <div className="navbar-container">
+      <div className="navbar"></div>
+    </div>
+  );
+};
+
+export default TopNavBar;
